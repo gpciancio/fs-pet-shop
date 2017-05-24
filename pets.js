0a2082cd3cd203100d30a8356e586403cb58035d
Elizabeth Flournoy <bflournoy@gmail.com>
2017-05-23 20:19:38 -0600

    first exercise completed

--- pets.js
+++ pets.js
@@ -2,17 +2,39 @@ const fs = require('fs');
 
 var pets = JSON.parse(fs.readFileSync('pets.json', 'utf8'));
 
-var method = process.argv[2]
-var index = process.argv[3]
+var method = process.argv[2];
 
 
 if (method === 'read'){
+  var index = process.argv[3];
   if (index>=0){
     console.log(pets[index]);
   }else{
     console.log(pets);
   }
-} else {
+}
+else if (method === 'create'){
+  var age = process.argv[3];
+  var kind = process.argv[4];
+  var name = process.argv[5];
+  if (!age || !kind || !name){
+    console.error(`Usage: node pets.js create AGE KIND NAME`);
+    process.exit(1);
+  } else {
+    var newPetObj = {};
+    newPetObj.age = Number(age);
+    newPetObj.kind = kind;
+    newPetObj.name = name;
+    pets.push(newPetObj);
+    fs.writeFile('pets.json', JSON.stringify(pets), function (writeErr){
+      if (writeErr){
+        throw writeErr;
+      }
+        console.log(newPetObj);
+    })
+  }
+}
+else {
   console.error(`Usage: node pets.js [read | create | update | destroy]`);
   process.exit(1);
 }
